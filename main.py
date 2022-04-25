2b27fa41eb3d573cee4ab5d097a71d35fad0798d
Ilya Zhilov <zhilov702@gmail.com>
2022-04-25 20:27:15 +0300

    checking is user member

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
 from random import randint
 
 import data
@@ -8,6 +10,7 @@ class DiaryVkBot:
     def __init__(self, token: str) -> None:
         """Initialization"""
         self.vk_session = vk_api.VkApi(token=token)
+        self.long_poll = VkLongPoll(vk=self.vk_session)
 
     def send_message(self, user_id: int, message: str) -> None:
         """Send message to user"""
@@ -16,11 +19,34 @@ class DiaryVkBot:
             {
                 "user_id": user_id,
                 "message": message,
-                "random_id": randint(0, 2 ** 32)
+                "random_id": randint(0, 2 ** 10)
             }
         )
+
+    def is_member(self, user_id: int) -> int:
+        """Check is user member of the group"""
+        is_member = self.vk_session.method(
+            "groups.isMember",
+            {
+                "group_id": data.GROUP_ID,
+                "user_id": user_id
+            }
+        )
+
+        return is_member
+
+    def listen(self) -> None:
+        """Listening events"""
+        for event in self.long_poll.listen():
+            if event.type == VkEventType.MESSAGE_NEW:
+                if event.from_user:
+                    if event.to_me:
+                        if self.is_member(user_id=event.user_id):
+                            pass
+                        else:
+                            self.send_message(user_id=event.user_id, message="Перед использованием бота подпишись на группу!")
 
 
 if __name__ == "__main__":
     my_bot = DiaryVkBot(token=data.TOKEN)
-    my_bot.send_message(user_id=data.ADMIN_ID, message="I am working")
+    my_bot.listen()
