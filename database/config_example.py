b6af515b294301bbfafdca09bc688bd80bbc1cfc
Ilya Zhilov <zhilovgithub@gmail.com>
2022-05-29 22:53:16 +0300

    creating db and table + DataBaseQueries

--- database/config_example.py
+++ database/config_example.py
@@ -3,3 +3,4 @@
 HOST = ""  # Write here the db host
 USER = ""  # Write here the db user
 PASSWORD = ""  # Write here the db password
+DATABASE_NAME = ""  # Write here the db name
--- database/database.py
+++ database/database.py
@@ -1,11 +1,45 @@
-import mysql.connector
+from mysql.connector import connect, Error
 
-from config import HOST, USER, PASSWORD
+from config import HOST, USER, PASSWORD, DATABASE_NAME
 
 
 class DataBase:
-    def __init__(self):
+    def __init__(self) -> None:
         """Initialization"""
         self.host = HOST
         self.user = USER
         self.password = PASSWORD
+
+        try:
+            with connect(
+                host=self.host,
+                user=self.user,
+                password=self.password
+            ) as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(DataBaseQueries.create_db_query)
+
+            with connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=DATABASE_NAME
+            ) as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(DataBaseQueries.create_table_query)
+                    connection.commit()
+
+        except Error as e:
+            print(e)
+
+
+class DataBaseQueries:
+    create_db_query = f"""CREATE DATABASE IF NOT EXISTS {DATABASE_NAME}"""
+
+    create_table_query = """CREATE TABLE IF NOT EXISTS User(
+        user_id int NOT NULL PRIMARY KEY
+    )"""
+
+
+if __name__ == '__main__':
+    db = DataBase()
