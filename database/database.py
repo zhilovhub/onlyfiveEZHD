b190ac7a9f30646610746ce9a57fefeb85d86eec
Ilya Zhilov <zhilovgithub@gmail.com>
2022-05-29 22:34:17 +0300

    Initialazied DataBase

--- database/database.py
+++ database/database.py
@@ -1,1 +1,11 @@
 import mysql.connector
+
+from config import HOST, USER, PASSWORD
+
+
+class DataBase:
+    def __init__(self):
+        """Initialization"""
+        self.host = HOST
+        self.user = USER
+        self.password = PASSWORD
