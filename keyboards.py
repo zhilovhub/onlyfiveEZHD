a70e7d2d909e5fabf86c76fb89cdf86157ed6806
Ilya Zhilov <zhilovgithub@gmail.com>
2022-06-05 22:54:58 +0300

    CANCEL KEYBOARD

--- keyboards.py
+++ keyboards.py
@@ -18,7 +18,11 @@ class KeyBoards:
     KEYBOARD_MENU.add_line()
     KEYBOARD_MENU.add_button(label="Обращение в тех. поддержку")
 
-    # CANCEL KEYBOARD
+    # CANCEL_BACK KEYBOARD
     KEYBOARD_CANCEL_BACK = VkKeyboard()
     KEYBOARD_CANCEL_BACK.add_button(label="На шаг назад")
     KEYBOARD_CANCEL_BACK.add_button(label="Отменить")
+
+    # CANCEL KEYBOARD
+    KEYBOARD_CANCEL = VkKeyboard()
+    KEYBOARD_CANCEL.add_button(label="Отменить")
