56582e3ed61e026bec0d0de23fb9ad4b63caf0fb
Ilya <zhilovgithub@gmail.com>
2022-06-09 14:47:06 +0300

    DataBase + self.connection in users.py

--- database.py
+++ database.py
@@ -2,7 +2,13 @@ from config import *
 
 
 class DataBase:
-    def __init__(self):
+    def __init__(self, connection: CMySQLConnection) -> None:
+        """Initialized"""
         self.host = HOST
         self.user = USER
         self.password = PASSWORD
+        self.database_name = DATABASE_NAME
+        self.connection = connection
+
+    def __del__(self):
+        self.connection.close()
