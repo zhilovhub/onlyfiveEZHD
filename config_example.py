2309941a27dcc7955e016a8404802c736e0b41f5
Ilya Zhilov <zhilovgithub@gmail.com>
2022-06-27 22:47:20 +0300

    config_example.py

---
+++ config_example.py
@@ -0,0 +1,28 @@
+# WRITE VALUES AND RENAME THIS FILE TO config.py
+
+from vk_api.keyboard import VkKeyboard  # import for Keyboards
+# import for main file
+from vk_api import VkApi
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardButton
+from vk_api.exceptions import VkApiError
+from random import randint, choice
+from datetime import datetime
+from re import search, fullmatch
+from json import loads, dumps
+# import for states file
+from enum import Enum
+# import for database file
+from mysql.connector import connect, Error
+from mysql.connector.connection_cext import CMySQLConnection
+
+# VK_api constants
+TOKEN = ""
+ADMINS_ID = []
+GROUP_ID = 0
+
+# DB constants
+HOST = ""
+USER = ""
+PASSWORD = ""
+DATABASE_NAME = ""
