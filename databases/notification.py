d14cc796f993dac7676219fff74b6663a1952985
Ilya <zhilovgithub@gmail.com>
2022-07-13 09:28:45 +0300

    new notification table

--- databases/notification.py
+++ databases/notification.py
@@ -6,6 +6,22 @@ class NotificationCommands(DataBase):
         """Initialization"""
         super().__init__(connection)
 
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(NotificationQueries.create_table_notification_query)
+
+        except Error as e:
+            print(e)
+
 
 class NotificationQueries:
-    pass
+    create_table_notification_query = """CREATE TABLE IF NOT EXISTS notification(
+        user_id INT,
+        classroom_id INT,
+        FOREIGN KEY (user_id) REFERENCES User (user_id) ON DELETE CASCADE,
+        FOREIGN KEY (classroom_id) REFERENCES Classroom (classroom_id) ON DELETE CASCADE,
+        
+        new_classmate BOOLEAN DEFAULT 1,
+        leave_classmate BOOLEAN DEFAULT 1,
+        requests BOOLEAN DEFAULT 1
+    )"""
