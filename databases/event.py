2a15857a9072dac07accc0025db6329a46cb3f1c
Ilya Zhilov <zhilovgithub@gmail.com>
2022-07-18 11:13:03 +0300

    creating tables event_diary and eveny

--- databases/event.py
+++ databases/event.py
@@ -1,4 +1,4 @@
-from databases import *
+from database import *
 
 
 class EventCommands(DataBase):
@@ -6,6 +6,29 @@ class EventCommands(DataBase):
         """Initialization"""
         super().__init__(connection)
 
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(EventQueries.create_table_event_diary_query)
+                cursor.execute(EventQueries.create_table_event_query)
+        except Error as e:
+            print(e)
+
 
 class EventQueries:
-    pass
+    create_table_event_diary_query = """CREATE TABLE IF NOT EXISTS event_diary(
+        event_diary_id INT NOT NULL UNIQUE PRIMARY KEY,
+        classroom_id INT,
+        
+        FOREIGN KEY (classroom_id) REFERENCES Classroom (classroom_id) ON DELETE CASCADE
+    )"""
+
+    create_table_event_query = """CREATE TABLE IF NOT EXISTS event(
+        event_id INT NOT NULL UNIQUE PRIMARY KEY,
+        event_diary_id INT,
+        
+        start_time DATETIME,
+        end_time DATETIME,
+        label TEXT,
+        
+        FOREIGN KEY (event_diary_id) REFERENCES event_diary (event_diary_id) ON DELETE CASCADE
+    )"""
