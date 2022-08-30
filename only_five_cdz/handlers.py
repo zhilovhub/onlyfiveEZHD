565364c1511a5bfe9d9187deec750f1a753de72f
Ilya Zhilov <zhilovgithub@gmail.com>
2022-08-30 13:36:04 +0300

    training_spec

--- only_five_cdz/handlers.py
+++ only_five_cdz/handlers.py
@@ -60,7 +60,10 @@ class Handlers:
     async def handler(self, user_id: int, url: str) -> None:
         """handling url"""
         try:
-            if "uchebnik.mos.ru" in url:
+            if "training_spec" in url:
+                await self.send_message(user_id, "Ты должен отправить ссылку на мэш-тест, который "
+                                                 "тебе задали в качестве дз")
+            elif "uchebnik.mos.ru" in url:
                 parsed_answers = await self.parse_answers(url)
                 answers_text = get_answers_text(parsed_answers)
                 await self.send_message(user_id, answers_text)
@@ -70,6 +73,7 @@ class Handlers:
         except Exception as e:
             await self.send_message(user_id, "Произошла какая-то ошибка. Информация подана администратору")
             await self.send_message(ADMIN_ID, str(e) + " " + str(user_id))
+            raise e
 
     async def parse_answers(self, url: str) -> list:
         """Parses answers"""
@@ -87,8 +91,8 @@ class Handlers:
                 return separated_url[separated_url.index('test_by_binding') + 1]
             elif 'homework' in separated_url:
                 return separated_url[separated_url.index('homework') + 1].split("?")[0]
-            elif "training_spec" in separated_url:
-                return separated_url[separated_url.index("training_spec") + 1]
+            # elif "training_spec" in separated_url:
+                # return separated_url[separated_url.index("training_spec") + 1]
 
         parse_url = 'https://uchebnik.mos.ru/exam/rest/secure/testplayer/group'
 
